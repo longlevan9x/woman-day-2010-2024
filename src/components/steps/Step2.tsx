f233646005c4cfd7837fc053ece8ce1cfeee7cf3
Long Van <longvan121996@gmail.com>
2024-10-20 10:49:57 +0900

    change iamge

--- src/components/steps/Step2.tsx
+++ src/components/steps/Step2.tsx
@@ -13,10 +13,23 @@ export function Step2({ onStepChange, nextStep }) {
     };
 
     useEffect(() => {
+        const imgUrls = [
+            'https://media-be.chewy.com/wp-content/uploads/2022/09/27095535/cute-dogs-pembroke-welsh-corgi.jpg',
+            'https://media-be.chewy.com/wp-content/uploads/2022/09/27095740/golden-retriever-cute-dogs.jpg',
+            'https://img.freepik.com/premium-photo/beautiful-cute-cat-wallpaper_662214-10222.jpg',
+            'https://i.pinimg.com/736x/c3/eb/1e/c3eb1e45a80ce92a94590fc16e73da4f.jpg',
+            'https://t3.ftcdn.net/jpg/05/80/74/04/360_F_580740494_9R9vqDM4zshhNeEMIFgCFYh24bVuqkNa.jpg',
+            'https://t3.ftcdn.net/jpg/06/52/36/24/360_F_652362438_L0HI3l0sWg8XzZUiSxOXxM4VvW8WGCpZ.jpg',
+            'https://i.pinimg.com/originals/35/43/cd/3543cd5693750c5a1bccdbbaa1267ae5.jpg',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4zE2914CNsoP5NAYA6TbHCnps_ogqU3szfA&s',
+            'https://png.pngtree.com/background/20230525/original/pngtree-shark-with-a-big-and-beautiful-smile-picture-image_2734824.jpg',
+            'https://t3.ftcdn.net/jpg/05/72/04/56/360_F_572045630_2nkDW7ZH6SYAYGZm2qgUTNWZrREWaePA.jpg',
+        ];
+
         const _imageList: ImageModel[] = [];
         for (let index = 0; index < 10; index++) {
             _imageList.push({
-                url: `/images/loves/love-${index + 1}.jpg`,
+                url: imgUrls[index],
                 id: Date.now(),
                 name: 'Love 1',
                 rotateDeg: index
